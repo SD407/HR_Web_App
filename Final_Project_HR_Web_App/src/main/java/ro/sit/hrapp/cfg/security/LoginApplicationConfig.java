f23e19e951b34d2ec56f5e548f6c52d37d37299f
SD407 <dragan.sorinvladut@gmail.com>
2016-06-25 21:25:44 +0300

    Added LoginApplicationConfig for future testing.

--- Final_Project_HR_Web_App/src/main/java/ro/sit/hrapp/cfg/security/LoginApplicationConfig.java
+++ Final_Project_HR_Web_App/src/main/java/ro/sit/hrapp/cfg/security/LoginApplicationConfig.java
@@ -1,12 +1,12 @@
-///*
-// * Final_Project_HR_Web_App
-// * ro.sit.hrapp.cfg.security  
-// * a.java
-// * 
-// *
-// * MADE FOR TRAINING PURPOSES.
-// *
-// */
+/*
+ * Final_Project_HR_Web_App
+ * ro.sit.hrapp.cfg.security  
+ * a.java
+ * 
+ *
+ * MADE FOR TRAINING PURPOSES.
+ *
+ */
 //package ro.sit.hrapp.cfg.security;
 //
 //import org.springframework.context.annotation.Bean;
@@ -14,6 +14,7 @@
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.context.annotation.Import;
 //import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
 //import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 //
 ///**
@@ -25,13 +26,21 @@
 //@ComponentScan({ "ro.sit.hrapp.cfg.security.*" })
 //@Import(value = { SecurityConfig.class })
 //public class LoginApplicationConfig {
+//	
 //	@Bean
 //	public FreeMarkerViewResolver viewResolver() {
 //		FreeMarkerViewResolver viewResolver = new FreeMarkerViewResolver();
 //		viewResolver.setCache(true);
-//		viewResolver.setPrefix("/templates/spring/");
+//		viewResolver.setPrefix("");
 //		viewResolver.setSuffix(".ftl");
 //		return viewResolver;
+//	}
+//	
+//	@Bean 
+//	public FreeMarkerConfigurer freemarkerConfig() { 
+//	    FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer(); 
+//	    freeMarkerConfigurer.setTemplateLoaderPath("/templates/spring/");
+//	    return freeMarkerConfigurer; 
 //	}
 //	
 //}
