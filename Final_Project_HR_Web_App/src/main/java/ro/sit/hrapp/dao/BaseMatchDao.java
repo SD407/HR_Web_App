8e9b9eeb76fdd9921694275324cf81d473a5b2ef
Sorin Dragan <dragan.sorinvladut@gmail.com>
2016-07-18 09:14:38 +0300

    Update BaseMatchDao.java

--- Final_Project_HR_Web_App/src/main/java/ro/sit/hrapp/dao/BaseMatchDao.java
+++ Final_Project_HR_Web_App/src/main/java/ro/sit/hrapp/dao/BaseMatchDao.java
@@ -7,6 +7,9 @@ import ro.sit.hrapp.domain.Candidate;
 import ro.sit.hrapp.domain.Company;
 
 public interface BaseMatchDao<T extends AbstractModel> {
+	
+	//You could've written the below code as follows
+	// List<T> findModel(T model);
 
 	List<Candidate> findCandidates(Company companies);
 	List<Company> findCompanies(Candidate candidate);
